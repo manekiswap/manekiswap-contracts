f8143fbdc9b75782e5679797476ee2edac57dc10
delena <delenamaneki@gmail.com>
2021-06-12 16:49:29 +0700

    Update dep

--- test/VestingVault-test.ts
+++ test/VestingVault-test.ts
@@ -10,16 +10,15 @@ describe("Test vesting by maneki token", async function () {
   let mnkToken: ManekiToken
   let owner: SignerWithAddress
   let admin: SignerWithAddress
-  let user: SignerWithAddress
-  let miner: SignerWithAddress
-  let burner: SignerWithAddress
+  let alice: SignerWithAddress
+  let bob: SignerWithAddress
   let value = ethers.utils.parseEther("12")
   let vestingContract: VestingVault
   let cap = ethers.utils.parseEther("30000002")
   let allow = ethers.utils.parseEther("1002")
 
   beforeEach(async function () {
-    ;[owner, admin, user, miner, burner] = await ethers.getSigners()
+    ;[owner, admin, alice, bob] = await ethers.getSigners()
 
     let mnkFactory = await ethers.getContractFactory("ManekiToken")
     mnkToken = (await upgrades.deployProxy(mnkFactory, [cap], {
@@ -35,17 +34,20 @@ describe("Test vesting by maneki token", async function () {
     await vestingContract.deployed()
 
     await mnkToken.mint(owner.address, allow)
-    await mnkToken.approve(vestingContract.address, allow)
 
-    console.log("Vesting at ", vestingContract.address)
+    console.log("Allowance before: ", (await mnkToken.allowance(owner.address, vestingContract.address)).toString())
+    // await mnkToken.approve(vestingContract.address, allow)
+    // console.log("Allowance after: ", (await mnkToken.allowance(owner.address, vestingContract.address)).toString())
+
+    await mnkToken.increaseAllowance(vestingContract.address, allow)
+    console.log("Allowance after increase allow: ", (await mnkToken.allowance(owner.address, vestingContract.address)).toString())
   })
+  //
 
   let grant = ethers.utils.parseEther("10")
   it("should emit event on grant", async function () {
-    await vestingContract.addTokenGrant(user.address, grant, 10, 10)
-
-    let allow = await vestingContract.getGrantAmount(user.address)
-
-    console.log("Graned: ", allow)
+    await vestingContract.addTokenGrant(alice.address, allow, 10, 10)
+    let grantAllow = await vestingContract.getGrantAmount(alice.address)
+    console.log("Graned: ", grantAllow.toString())
   })
 })
