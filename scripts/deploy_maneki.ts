fc0b07f2cbe9243c592d8b1538e54b54bb42b518
theplayer06 <manekiswap@gmail.com>
2021-06-21 12:30:32 +0700

    Replace 'it.only' by 'it' and format codes

--- scripts/deploy_maneki.ts
+++ scripts/deploy_maneki.ts
@@ -3,16 +3,17 @@ import { ethers, upgrades } from "hardhat"
 async function main() {
   const factory = await ethers.getContractFactory("ManekiToken")
   console.log("Deploying proxy...")
-  let cap = ethers.utils.parseUnits("100000000")
-  let contract = await upgrades.deployProxy(factory, [cap], { initializer: "initialize(uint256)" })
+  const cap = ethers.utils.parseUnits("100000000")
+  const contract = await upgrades.deployProxy(factory, [cap], { initializer: "initialize(uint256)" })
 
   await contract.deployed()
   console.log("Proxy maneki address: ", contract.address)
 
-  let vestingFactory = await ethers.getContractFactory("VestingVault")
-  let vestingContract = await upgrades.deployProxy(vestingFactory, [contract.address], {
+  const vestingFactory = await ethers.getContractFactory("VestingVault")
+  const vestingContract = await upgrades.deployProxy(vestingFactory, [contract.address], {
     initializer: "initialize(address)",
   })
+
   await vestingContract.deployed()
 
   console.log("Proxy vesting address: ", vestingContract.address)
