578eb297eb11c0e7a87afa4730e16e928213ea1f
delena <delenamaneki@gmail.com>
2021-06-28 11:23:42 +0700

    Sample token distribution in rinkeby

--- scripts/token_distribute_rinkeby/1_init_fund.ts
+++ scripts/token_distribute_rinkeby/1_init_fund.ts
@@ -1,11 +1,13 @@
-import { ethers, upgrades } from "hardhat"
+import { ethers } from "hardhat"
 
-import { ManekiToken, VestingVault } from "../../src/typechain"
+import { ManekiToken } from "../../src/typechain"
+
+// const vaultAddr = "0xc4b812dd94F6a7035334e5618c282Ce0079de2e2"
+// const manekiAddr = "0x7DBa9Cb61BAC2c0eD775f688Ac021191AB207dCc"
 
 const vaultAddr = "0x57d946727Ac6E0E22d8Ef7D376cbE7ca8bf30731"
 const manekiAddr = "0x329F7876Fc3Fc45Afbb197727916217eD3C052b2"
 
-const founder = "0x6722e5fafd680f15F1c4BF3D0De50338E7E015E9"
 const owner = "0x916f2A376afB8fF523Ed6910217a5E0EFbF027Cc"
 const treasury = "0x3C9255Ca23578dbf2401340Eb6eea4ae114E3C2c"
 const community = "0x0D849AA4b74f9Ec3D59ceD55C0bB445771BcE973"
@@ -15,11 +17,11 @@ async function main() {
   const MANEKI = await ethers.getContractFactory("ManekiToken")
   const contract = MANEKI.attach(manekiAddr) as ManekiToken
 
-  // tranfer to fouder
   // mint to treasury  25%  * 100M => 25M
   // mint to community 35% => 35M
-  // mint to owner address 25% (founder (15M) + team(10M)) =< 25M
   // mint to team 5% => 5M
+
+  // mint to owner address (to approve vault) 25% (founder (15M) + team(10M)) => 25M
   // not mint yet: 10% => 10M remain (investor)
 
   const txTreasury = await contract.mint(treasury, ethers.utils.parseUnits("25000000")) // 25M
