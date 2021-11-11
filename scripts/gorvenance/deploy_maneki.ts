cd97170e236f0c5b87f1f906e4abc6783cf6577f
theplayer06 <manekiswap@gmail.com>
2021-11-11 16:01:10 +0700

    Grant and revoke roles by script

--- scripts/gorvenance/deploy_maneki.ts
+++ scripts/gorvenance/deploy_maneki.ts
@@ -3,7 +3,10 @@ import { ethers, upgrades } from "hardhat"
 import { ManekiToken, VestingVault } from "../../src/typechain"
 
 async function main() {
+  const signers = await ethers.getSigners()
+  const owner = signers[0]
   const multisig_address = process.env.MULTISIG_ADDRESS || ""
+  console.log("Multisig address:  ", multisig_address)
 
   const factory = await ethers.getContractFactory("ManekiToken")
   console.log("Deploying proxy...")
@@ -21,7 +24,20 @@ async function main() {
 
   await vestingContract.deployed()
   console.log("Proxy vesting address:  ", vestingContract.address)
-  console.log("Multisig address:  ", multisig_address)
+
+  const defaultAdminROLE = await manekiContract.DEFAULT_ADMIN_ROLE()
+  const minterROLE = await manekiContract.MINTER_ROLE()
+  const pauseROLE = await manekiContract.PAUSER_ROLE()
+
+  console.log("Granting roles to multisigAddress")
+  await manekiContract.grantRole(defaultAdminROLE, multisig_address)
+  await manekiContract.grantRole(minterROLE, multisig_address)
+  await manekiContract.grantRole(pauseROLE, multisig_address)
+
+  console.log("Revoking roles from deployer")
+  await manekiContract.revokeRole(pauseROLE, owner.address)
+  await manekiContract.revokeRole(minterROLE, owner.address)
+  await manekiContract.revokeRole(defaultAdminROLE, owner.address)
 }
 
 main()
